3bc8b4a034225866082a279355c7a38c59433bc7
zzzzzzasd123 <112676929+zzzzzzasd123@users.noreply.github.com>
2022-11-11 22:37:22 +0900

    Create turtle2.py

---
+++ turtle2.py
@@ -0,0 +1,14 @@
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+for i in range(10):
+    t.forward(-100)
+    t.lt(36)
+    t.forward(100)
+    t.lt(36)
+    t.forward(-100)
+    t.lt(36)
+    t.forward(100)
+    t.lt(36)
+    t.forward(-100)
+    t.rt(134)
